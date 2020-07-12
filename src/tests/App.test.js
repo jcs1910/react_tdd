08b951f1ec6242c3bac69e6c5487dd52f4ca6f6b
dokdo <jcs191072@gmail.com>
2020-07-12 14:03:28 +0900

    first commit

---
+++ src/tests/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('App', function () {
+    const app = shallow(<App />);
+
+    it('renders properly', function () {
+        expect(app).toMatchSnapshot();
+    });
+});
