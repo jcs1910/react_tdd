079dd2c39b3d5b4367deb7585a64e9498ac2c7dd
dokdo <jcs191072@gmail.com>
2020-07-12 14:06:03 +0900

    Start up LootCheck with an App component and test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
+import { shallow, configure } from 'enzyme';
+import App from '../components/App';
+
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('App', function () {
     const app = shallow(<App />);
---
+++ src/tests/balance.test.js
@@ -0,0 +1,10 @@
+import * as constants from '../actions/constants';
+import * as actions from '../actions/balance';
+
+it('create an action to set the balance', function () {
+    const balance = 0;
+
+    const expectedAction = { type: constants.SET_BALANCE, balance };
+
+
+});
